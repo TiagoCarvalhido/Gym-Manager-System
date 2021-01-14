394d1e9e9b5dff62f259d7a194e912f6dc062774
Tiago <tcarvalhido@gmail.com>
2021-01-14 18:28:59 +0000

    first commit

---
+++ API/models/Client.model.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, Sequelize) => {
+    const Client = sequelize.define("Cliente", {
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      initialWeight: {
+        type: Sequelize.FLOAT
+      },
+      
+      currentlWeight: {
+        type: Sequelize.FLOAT
+      },
+
+      monthlyPayed: {
+        type: Sequelize.BOOLEAN
+      }
+    });
+  
+    return Client;
+  };
